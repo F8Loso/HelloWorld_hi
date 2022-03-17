ee5d1768ac48a0957767d66801f18583ed5cd747
Karlos Ferbos <99383647+F8Loso@users.noreply.github.com>
2022-03-17 09:59:26 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 2; i<=50;i+=2){
+			System.out.println(i);
+		}
+
+	}
+}
